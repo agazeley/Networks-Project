9d4f7fd0ae384c53db140d3ca9aa68c6cf5214a8
Andrew W Gazeley <agazeley18@my.whitworth.edu>
2018-02-07 12:39:30 -0800

    working on implementing Client_game_WORKING.py classes into c# Client.cs

--- Battleship_Client/Battleship_Client/Client.cs
+++ Battleship_Client/Battleship_Client/Client.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace Battleship_Client
 {
     class Client
     {
         private int server_port;
-        private string server_ip;
+        private IPAddress server_ip;
         private int msg_size = 2048;
         private UdpClient client;
+        private IPEndPoint ep_server;
 
         public Client()
         {
@@ -24,21 +26,39 @@ namespace Battleship_Client
         public Client(string ip,int port)
         {
             this.client = new UdpClient();
-            this.server_ip = ip;
+            this.server_ip = IPAddress.Parse(ip);
             this.server_port = port;
+            this.ep_server = new IPEndPoint(this.server_ip, port);
         }
 
         public bool start_client(string name)
         {
             Debug.WriteLine("Connecting to " + this.server_ip + ": " + this.server_port.ToString());
             string data = create_request(name, "connect", "1");
+            byte[] byte_data = Encoding.ASCII.GetBytes(data);
 
-            if (send(data, this.server_ip, this.server_port))
+            try
             {
+                this.client.Send(byte_data, byte_data.Length, this.ep_server);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+
+            byte[] server_message = this.client.Receive(ref this.ep_server);
+            string json = Encoding.ASCII.GetString(server_message);
+            var msg = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            if (msg["type"] == "conn_request" && int.Parse(msg["msg"]) == 1)
+            {
+                // Successful handshake!
                 return true;
             }
             else
             {
+                // exit(0)?
                 return false;
             }
         }
@@ -57,12 +77,12 @@ namespace Battleship_Client
             }
         }
 
-        public bool send(string data,string ip,int port)
+        public bool send(string data,IPEndPoint ep)
         {
             byte[] byte_data = Encoding.ASCII.GetBytes(data);
             try
             {
-                this.client.Send(byte_data, byte_data.Length, ip, port);
+                this.client.Send(byte_data, byte_data.Length, ep);
                 return true;
             }
             catch(Exception e)
@@ -71,6 +91,22 @@ namespace Battleship_Client
                 return false;
             }
         }
+        public bool server_request(string data)
+        {
+            byte[] byte_data = Encoding.ASCII.GetBytes(data);
+
+            try
+            {
+                this.client.Send(byte_data, byte_data.Length, this.ep_server);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
 
         // Need to make it so id is optional field
         public string create_request(string player, string type,string req, int id)
